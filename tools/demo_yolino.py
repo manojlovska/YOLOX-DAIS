a804ae75f343042d9e93cdf337c8bbcb5363b52b
manojlovska <annastasijamanojlovska@gmail.com>
2023-12-05 10:20:54 +0000

    Added image demo for yolino

--- tools/demo_yolino.py
+++ tools/demo_yolino.py
@@ -11,6 +11,7 @@ import cv2
 
 import torch
 
+from visualize_yolino import Visualizer
 from yolox.data.data_augment import ValTransform
 from yolox.data.datasets import DAIS_CLASSES
 from yolox.exp import get_exp
@@ -156,25 +157,27 @@ class Predictor(object):
             logger.info("Infer time: {:.4f}s".format(time.time() - t0))
         return outputs, img_info
 
-    def visual(self, output, img_info, cls_conf=0.35):
+    def visual(self, output, image_name, img_info, cls_conf=0.35):
         ratio = img_info["ratio"]
         img = img_info["raw_img"]
         if output is None:
             return img
+        
         output = output.cpu()
+        # import pdb; pdb.set_trace()
 
         output = output.reshape(20, 20, 5)
-
-        coords_pred = output[:, :, :4] # Outputs of the network are in shape [400, 1, 5], predicted coordinates are [400, 1, 4]
-        conf_pred = output[:, :, -1]
-
-        # preprocessing: resize
-        # coords_pred /= ratio NE ZNAM DALI TREBA
-
+        # import pdb; pdb.set_trace()
         
+        image = Visualizer(image_name, self.test_size[0], self.test_size[1], 32)
+        polylines = image.get_polylines("/home/manojlovska/Documents/YOLOX/datasets/DAIS-COCO/annotations_xml/annotations.xml")
+        image.draw_grid()
+        image.draw_cartesian_predictors(output)
+        image.draw_true_polylines(polylines)
+        image.draw_cartesian_intersections(output)
+        image.show_image()
 
-        # vis_res = vis(img, bboxes, scores, cls, cls_conf, self.cls_names)
-        return output
+        return image
     
 
 def image_demo(predictor, vis_folder, path, current_time, save_result):
@@ -185,7 +188,7 @@ def image_demo(predictor, vis_folder, path, current_time, save_result):
     files.sort()
     for image_name in files:
         outputs, img_info = predictor.inference(image_name)
-        result_image = predictor.visual(outputs[0], img_info, predictor.confthre)
+        result_image = predictor.visual(outputs[0], image_name, img_info, predictor.confthre)
         if save_result:
             save_folder = os.path.join(
                 vis_folder, time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
@@ -193,7 +196,87 @@ def image_demo(predictor, vis_folder, path, current_time, save_result):
             os.makedirs(save_folder, exist_ok=True)
             save_file_name = os.path.join(save_folder, os.path.basename(image_name))
             logger.info("Saving detection result in {}".format(save_file_name))
+            import pdb; pdb.set_trace()
             cv2.imwrite(save_file_name, result_image)
+            result_image.save_image(save_folder, os.path.basename(image_name))
         ch = cv2.waitKey(0)
         if ch == 27 or ch == ord("q") or ch == ord("Q"):
             break
+
+def main(exp, args):
+    if not args.experiment_name:
+        args.experiment_name = exp.exp_name
+
+    file_name = os.path.join(exp.output_dir, args.experiment_name)
+    os.makedirs(file_name, exist_ok=True)
+
+    vis_folder = None
+    if args.save_result:
+        vis_folder = os.path.join(file_name, "vis_res")
+        os.makedirs(vis_folder, exist_ok=True)
+
+    if args.trt:
+        args.device = "gpu"
+
+    logger.info("Args: {}".format(args))
+
+    if args.conf is not None:
+        exp.test_conf = args.conf
+    if args.nms is not None:
+        exp.nmsthre = args.nms
+    if args.tsize is not None:
+        exp.test_size = (args.tsize, args.tsize)
+
+    model = exp.get_model()
+    logger.info("Model Summary: {}".format(get_model_info(model, exp.test_size)))
+
+    if args.device == "gpu":
+        model.cuda()
+        if args.fp16:
+            model.half()  # to FP16
+    model.eval()
+
+    if not args.trt:
+        if args.ckpt is None:
+            ckpt_file = os.path.join(file_name, "best_ckpt.pth")
+        else:
+            ckpt_file = args.ckpt
+        logger.info("loading checkpoint")
+        ckpt = torch.load(ckpt_file, map_location="cpu")
+        # load the model state dict
+        model.load_state_dict(ckpt["model"])
+        logger.info("loaded checkpoint done.")
+
+    if args.fuse:
+        logger.info("\tFusing model...")
+        model = fuse_model(model)
+
+    if args.trt:
+        assert not args.fuse, "TensorRT model is not support model fusing!"
+        trt_file = os.path.join(file_name, "model_trt.pth")
+        assert os.path.exists(
+            trt_file
+        ), "TensorRT model is not found!\n Run python3 tools/trt.py first!"
+        model.head.decode_in_inference = False
+        decoder = model.head.decode_outputs
+        logger.info("Using TensorRT to inference")
+    else:
+        trt_file = None
+        decoder = None
+
+    predictor = Predictor(
+        model, exp, trt_file,
+        args.device, args.fp16, args.legacy,
+    )
+    current_time = time.localtime()
+    if args.demo == "image":
+        image_demo(predictor, vis_folder, args.path, current_time, args.save_result)
+    # elif args.demo == "video" or args.demo == "webcam":
+    #     imageflow_demo(predictor, vis_folder, current_time, args)
+
+
+if __name__ == "__main__":
+    args = make_parser().parse_args()
+    exp = get_exp(args.exp_file, args.name)
+
+    main(exp, args)
